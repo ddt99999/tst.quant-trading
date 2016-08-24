e4caff2586997ee46f06af8c87c8f874bfb4063f
Tzyy Tong <Tzyy-shyang.Tong@ectp.com>
2016-08-24 19:44:46 +0100

    Added Winter 2012-2013.kml and PythonAsFunctionalProgramming.py chapter 4 exercises

--- QuantTrading/QuantLib/Examples/PythonAsFunctionalProgramming.py
+++ QuantTrading/QuantLib/Examples/PythonAsFunctionalProgramming.py
@@ -135,3 +135,147 @@ for subset in sample_I, sample_II, sample_III, sample_III:
     print(len(subset))
     mean = sum(float(pair.y) for pair in subset)/len(subset)
     print(mean)
+    
+'''
+Chapter 4 - Collections
+
+Summary
+=======
+One common application of for loop iterable processing is the
+unwrap(process(wrap(iterable))) design pattern. A wrap() function will first
+transform each item of an iterable into a two tuples with a derived sort key or other
+value and then the original immutable item. We can then process these two tuples
+based on the wrapped value. Finally, we'll use an unwrap() function to discard the
+value used to wrap, which recovers the original item. 
+This happens so often in a functional context that we have two functions that are
+used heavily for this; they are as follows:
+
+fst = lambda x: x[0]
+snd = lambda x: x[1]
+
+These two functions pick the first and second values from a tuple, and both are
+handy for the process() and unwrap() functions.
+
+Another common pattern is wrap(wrap(wrap())). In this case, we're starting
+with simple tuples and then wrapping them with additional results to build
+up larger and more complex tuples. A common variation on this theme is
+extend(extend(extend())) where the additional values build new, more
+complex namedtuple instances without actually wrapping the original tuples.
+We can summarize both of these as the Accretion design pattern.
+
+We'll apply the Accretion design to work with a simple sequence of latitude and
+longitude values. The first step will convert the simple points (lat, lon) on a path
+into pairs of legs (begin, end). Each pair in the result will be ((lat, lon), (lat, lon)).
+In the next sections, we'll show how to create a generator function that will iterate over
+the content of a file. This iterable will contain the raw input data that we will process.
+Once we have the data, later sections will show how to decorate each leg with the
+haversine distance along the leg. The final result of the wrap(wrap(iterable())))
+processing will be a sequence of three tuples: ((lat, lon), (lat, lon), distance). We
+can then analyze the results for the longest, shortest distance, bounding rectangle,
+and other summaries of the data.
+'''
+
+import xml.etree.ElementTree as XML
+import urllib
+
+# Applying generator expressions to scalar functions
+from math import radians, sin, cos, sqrt, asin
+
+MI = 3959
+NM = 3440
+KM = 6371
+
+def haversine(point1, point2, R=NM):
+    lat1, lon1 = point1
+    lat2, lon2 = point2
+    
+    delta_lat = radians(lat2 - lat1)
+    delta_lon = radians(lon2 - lon1)
+    
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    a = sin(delta_lat/2)**2 + cos(lat1)*cos(lat2)*sin(delta_lon/2)**2
+    c = 2*asin(sqrt(a))
+    
+    return R*c
+
+def row_iter_kml(file_obj):
+    ns_map = {
+        "ns0": "http://www.opengis.net/kml/2.2",    
+        "ns1": "http://www.google.com/kml/ext/2.2"    
+    }
+    doc = XML.parse(file_obj)
+    return (comma_split(coordinates.text) 
+            for coordinates in 
+            doc.findall("./ns0:Document/ns0:Folder/ns0:Placemark/ns0:Point/ns0:coordinates", ns_map))
+            
+def comma_split(text):
+    return text.split(",")
+    
+def pick_lat_lon(lon, lat, alt):
+    return lat, lon
+    
+def lat_lon_kml(row_iter):
+    return (pick_lat_lon(*row) for row in row_iter)
+    
+# parse the sequence recursively
+def pairs(iterable):
+    def pair_from(curr_item, iterable_tail):
+        next_item = next(iterable_tail)
+        yield curr_item, next_item
+        yield from pair_from(next_item, iterable_tail)
+    try:
+        return pair_from(next(iterable), iterable)
+    except StopIteration:
+        return
+        
+def float_from_pair(lat_lon_iter):
+    return ((float(lat), float(lon)) for lat, lon in lat_lon_iter)
+        
+# tail-call optimization
+def legs(lat_lon_iter):
+    begin = next(lat_lon_iter)
+    for end in lat_lon_iter:
+        yield begin, end
+        begin = end
+ 
+  
+with urllib.request.urlopen("file:./Winter%202012-2013.kml") as source:
+    flts = tuple(legs(float_from_pair(lat_lon_kml(row_iter_kml(source))))) 
+    print("FLOATS")
+    print(flts)
+    source.seek(0)
+    print("STRINGS")
+    v1 = tuple(lat_lon_kml(row_iter_kml(source)))
+    print(v1)
+    source.seek(0)
+    trip = ((start, end, round(haversine(start, end), 4)) 
+            for start, end in legs(float_from_pair(lat_lon_kml(row_iter_kml(source)))))
+
+
+for start, end, dist in trip:
+    print(start, end, dist)    
+
+n = 10
+flat = ['2', '3', '5', '7', '11', '13', '17', '19', '23', '29', '31', '37', '41', '43', '47', '53', '59', '61', '67', '71' ]
+for elem in range(n):
+    print(flat[elem::n])
+test = list(zip(*(flat[i::n] for i in range(n))))
+
+# Using reversed() to change the order
+def digits(x, b):
+    if x == 0:
+        return
+    yield x % b
+    yield from digits(x//b,b)
+#    for d in to_base(x//b, b):
+#        yield d
+        
+def to_base(x, b):
+    return digits(x, b)
+    
+test = reversed(list((digits(12,2))))
+    
+for elem in test:
+    print(elem)
