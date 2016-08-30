4aad40d87d6713d19ac1cd806522573d2a0b0341
Tzyy Tong <Tzyy-shyang.Tong@ectp.com>
2016-08-30 17:44:02 +0100

    Finished chapter 7 for PythonAsFunctionalProgramming.py

--- QuantTrading/QuantLib/Examples/Python_As_Functional/PythonAsFunctionalProgramming.py
+++ QuantTrading/QuantLib/Examples/Python_As_Functional/PythonAsFunctionalProgramming.py
@@ -619,6 +619,7 @@ longitude= lambda lat, lon: lon
 # namedtuple functions such as the following:
 Leg = namedtuple("Leg", ("start", "end", "distance"))
 Point = namedtuple("Point", ("latitude", "longitude"))
+Pair = namedtuple("Pair", ("x","y"))
 
 # Avoiding stateful classes by using families of tuples
 
@@ -672,3 +673,252 @@ xy_rank= tuple(enumerate(sorted(y_rank, key=lambda rank: rank[1].x)))
 x_rank = lambda ranked: ranked[0]
 y_rank= lambda ranked: ranked[1][0]
 raw = lambda ranked: ranked[1][1]
+
+# Assigning statistical ranks 
+from collections import defaultdict
+
+def rank(data, key=lambda obj: obj):
+    def rank_output(duplicates, key_iter, base=0):
+        for k in key_iter:
+            dups = len(duplicates[k])
+            for value in duplicates[k]:
+                yield (base+1+base+dups)/2, value
+            base += dups
+        
+    def build_duplicates(duplicates, data_iter, key):
+        for item in data_iter:
+            duplicates[key(item)].append(item)
+        return duplicates
+    
+    duplicates = build_duplicates(defaultdict(list), iter(data), key)
+    return rank_output(duplicates, iter(sorted(duplicates)), 0)
+    
+ranks = list(rank([0.8, 1.2, 1.2, 2.3, 18]))
+
+data= ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
+ranks_2 = list(rank(data, key=lambda x:x[1]))
+
+def rank2_imp( data, key= lambda x:x ):
+    """
+    Alternative rank using a stateful queue object: optimized version.
+
+    >>> list(rank2_imp( [0.8, 1.2, 1.2, 2.3, 18] ) )
+    [(1.0, 0.8), (2.5, 1.2), (2.5, 1.2), (4.0, 2.3), (5.0, 18)]
+    >>> data= ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
+    >>> list(rank2_imp( data, key=lambda x:x[1] ))
+    [(1.0, (2, 0.8)), (2.5, (3, 1.2)), (2.5, (5, 1.2)), (4.0, (7, 2.3)), (5.0, (11, 18))]
+    """
+    data_iter= iter(sorted(data, key=key))
+    base= 0
+    same_rank= [next(data_iter)] # Queue().append(data_iter)
+    for value in data_iter:
+        if key(value) == key(same_rank[0]):
+            same_rank.append( value ) # or same_rank += [value]
+        else:
+            dups= len(same_rank)
+            for dup_rank_item in same_rank: # same_rank.pop()
+                yield (base+1+base+dups)/2, dup_rank_item
+            base += dups
+            same_rank = [value] # same_rank.append()
+    dups= len(same_rank)
+    for value in same_rank: # same_rank.pop()
+        yield (base+1+base+dups)/2, value
+
+def rank2_rec( data, key= lambda x:x ):
+    """
+    Alternative rank without a Counter object.
+    Closer to properly recursive version.
+
+    >>> list(rank2_rec( [0.8, 1.2, 1.2, 2.3, 18] ) )
+    [(1.0, 0.8), (2.5, 1.2), (2.5, 1.2), (4.0, 2.3), (5.0, 18)]
+    >>> data= ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
+    >>> list(rank2_rec( data, key=lambda x:x[1] ))
+    [(1.0, (2, 0.8)), (2.5, (3, 1.2)), (2.5, (5, 1.2)), (4.0, (7, 2.3)), (5.0, (11, 18))]
+    """
+    def yield_sequence( rank, same_rank_iter ):
+        head= next(same_rank_iter)
+        yield rank, head
+        #for rest in yield_sequence( rank, same_rank_iter ):
+        #    yield rest
+        yield from  yield_sequence( rank, same_rank_iter )
+
+    def ranker( sorted_iter, base, same_rank_seq ):
+        try:
+            value= next(sorted_iter)
+        except StopIteration:
+            dups= len(same_rank_seq)
+            #for rest in yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) ):
+            #    yield rest
+            yield from yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) )
+            return
+        if key(value) == key(same_rank_seq[0]):
+            #for rows in ranker(sorted_iter, base, same_rank_seq+[value] ):
+            #    yield rows
+            yield from ranker(sorted_iter, base, same_rank_seq+[value] )
+        else:
+            dups= len(same_rank_seq)
+            #for rest in yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) ):
+            #    yield rest
+            yield from yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) )
+            for rows in ranker(sorted_iter, base+dups, [value]): yield rows
+
+    data_iter= iter(sorted(data, key=key))
+    head = next(data_iter)
+    #for rows in ranker( data_iter, 0, [head] ): yield rows
+    yield from ranker( data_iter, 0, [head] )
+
+Ranked_Y= namedtuple("Ranked_Y", ("r_y", "raw",) )
+
+def rank_y( pairs ):
+    """
+    Partial rank ordering of a data set by the y member of each pair.
+
+    >>> data = (Pair(x=10.0, y=8.04), Pair(x=8.0, y=6.95), Pair(x=13.0, y=7.58), Pair(x=9.0, y=8.81), Pair(x=11.0, y=8.33), Pair(x=14.0, y=9.96), Pair(x=6.0, y=7.24), Pair(x=4.0, y=4.26), Pair(x=12.0, y=10.84), Pair(x=7.0, y=4.82), Pair(x=5.0, y=5.68))
+    >>> list(rank_y(data))
+    [Ranked_Y(r_y=1.0, raw=Pair(x=4.0, y=4.26)), Ranked_Y(r_y=2.0, raw=Pair(x=7.0, y=4.82)), Ranked_Y(r_y=3.0, raw=Pair(x=5.0, y=5.68)), Ranked_Y(r_y=4.0, raw=Pair(x=8.0, y=6.95)), Ranked_Y(r_y=5.0, raw=Pair(x=6.0, y=7.24)), Ranked_Y(r_y=6.0, raw=Pair(x=13.0, y=7.58)), Ranked_Y(r_y=7.0, raw=Pair(x=10.0, y=8.04)), Ranked_Y(r_y=8.0, raw=Pair(x=11.0, y=8.33)), Ranked_Y(r_y=9.0, raw=Pair(x=9.0, y=8.81)), Ranked_Y(r_y=10.0, raw=Pair(x=14.0, y=9.96)), Ranked_Y(r_y=11.0, raw=Pair(x=12.0, y=10.84))]
+    """
+    return (Ranked_Y(*row)
+            for row in rank( pairs, lambda pair: pair.y ) )
+                
+    
+
+Ranked_XY= namedtuple("Ranked_XY", ("r_x", "r_y", "raw",) )
+
+def rank_xy( pairs ):
+    """
+    Two-pass rank ordering by the x and y members of each pair.
+    An intermediate sequence of Rank_Y objects are created with partial
+    rankings. The result are Rank_XY objects.
+
+    >>> data = (Pair(x=10.0, y=8.04), Pair(x=8.0, y=6.95), Pair(x=13.0, y=7.58), Pair(x=9.0, y=8.81), Pair(x=11.0, y=8.33), Pair(x=14.0, y=9.96), Pair(x=6.0, y=7.24), Pair(x=4.0, y=4.26), Pair(x=12.0, y=10.84), Pair(x=7.0, y=4.82), Pair(x=5.0, y=5.68))
+    >>> list(rank_xy(data))
+    [Ranked_XY(r_x=1.0, r_y=1.0, raw=Pair(x=4.0, y=4.26)), Ranked_XY(r_x=2.0, r_y=3.0, raw=Pair(x=5.0, y=5.68)), Ranked_XY(r_x=3.0, r_y=5.0, raw=Pair(x=6.0, y=7.24)), Ranked_XY(r_x=4.0, r_y=2.0, raw=Pair(x=7.0, y=4.82)), Ranked_XY(r_x=5.0, r_y=4.0, raw=Pair(x=8.0, y=6.95)), Ranked_XY(r_x=6.0, r_y=9.0, raw=Pair(x=9.0, y=8.81)), Ranked_XY(r_x=7.0, r_y=7.0, raw=Pair(x=10.0, y=8.04)), Ranked_XY(r_x=8.0, r_y=8.0, raw=Pair(x=11.0, y=8.33)), Ranked_XY(r_x=9.0, r_y=11.0, raw=Pair(x=12.0, y=10.84)), Ranked_XY(r_x=10.0, r_y=6.0, raw=Pair(x=13.0, y=7.58)), Ranked_XY(r_x=11.0, r_y=10.0, raw=Pair(x=14.0, y=9.96))]
+    """
+    return (Ranked_XY(r_x=r_x, r_y=rank_y_raw[0], raw=rank_y_raw[1])
+            for r_x, rank_y_raw in rank( rank_y(pairs), lambda r: r.raw.x ) )
+
+data = (Pair(x=10.0, y=8.04), Pair(x=8.0, y=6.95), Pair(x=13.0, y=7.58), Pair(x=9.0, y=8.81), Pair(x=11.0, y=8.33), Pair(x=14.0, y=9.96), Pair(x=6.0, y=7.24), Pair(x=4.0, y=4.26), Pair(x=12.0, y=10.84), Pair(x=7.0, y=4.82), Pair(x=5.0, y=5.68))
+rank_xy_list = list(rank_xy(data))
+
+def rank_corr( pairs ):
+    """Spearman rank correlation.
+    >>> data = [Pair(x=86.0, y=0.0), Pair(x=97.0, y=20.0), Pair(x=99.0, y=28.0), Pair(x=100.0, y=27.0), Pair(x=101.0, y=50.0), Pair(x=103.0, y=29.0), Pair(x=106.0, y=7.0), Pair(x=110.0, y=17.0), Pair(x=112.0, y=6.0), Pair(x=113.0, y=12.0)]
+    >>> round(rank_corr( data ), 9)
+    -0.175757576
+    >>> data = (Pair(x=10.0, y=8.04), Pair(x=8.0, y=6.95), Pair(x=13.0, y=7.58), Pair(x=9.0, y=8.81), Pair(x=11.0, y=8.33), Pair(x=14.0, y=9.96), Pair(x=6.0, y=7.24), Pair(x=4.0, y=4.26), Pair(x=12.0, y=10.84), Pair(x=7.0, y=4.82), Pair(x=5.0, y=5.68))
+    >>> round(rank_corr( data ), 3)
+    0.818
+
+    Note that Pearson R for Anscombe data set I is 0.816.
+    The difference, while small, is significant.
+
+    >>> hgt_mass= (Pair(x=1.47, y=52.21), Pair(x=1.5, y=53.12), Pair(x=1.52, y=54.48), Pair(x=1.55, y=55.84), Pair(x=1.57, y=57.2), Pair(x=1.6, y=58.57), Pair(x=1.63, y=59.93), Pair(x=1.65, y=61.29), Pair(x=1.68, y=63.11), Pair(x=1.7, y=64.47), Pair(x=1.73, y=66.28), Pair(x=1.75, y=68.1), Pair(x=1.78, y=69.92), Pair(x=1.8, y=72.19), Pair(x=1.83, y=74.46))
+    >>> round(rank_corr( hgt_mass ), 3)
+    1.0
+    """
+    ranked= rank_xy( pairs )
+    sum_d_2 = sum( (r.r_x - r.r_y)**2 for r in ranked )
+    n = len(pairs)
+    return 1-6*sum_d_2/(n*(n**2-1))
+    
+pearson_rank_corr = round(rank_corr( data ), 3)
+
+
+# Polymorphism and Pythonic pattern matching
+'''
+In Python, we're effectively writing generic definitions because the code isn't bound to any specific data type. 
+The Python runtime will locate the appropriate operations using a simple set of matching rules. The 3.3.7 Coercion rules section of the language
+reference manual and the numbers module in the library provide details on how this mapping from operation to special method name works.
+
+In rare cases, we might need to have different behavior based on the types of the data
+elements. We have two ways to tackle this; they are as follows:
+    •   We can use the isinstance() function to distinguish the different cases
+    •   We can create our own subclass of numbers.Number or tuple and implement a proper polymorphic special method names
+In some cases, we'll actually need to do both so that we can include appropriate data type conversions.
+'''
+Rank_Data = namedtuple( "Rank_Data", ("rank_seq", "raw") )
+
+def rank_data( seq_or_iter, key=lambda obj:obj ):
+    """Rank raw data by creating Rank_Data objects from an iterable.
+    Or rerank existing data by creating new Rank_Data objects from
+    old Rank_Data objects.
+
+    >>> scalars= [0.8, 1.2, 1.2, 2.3, 18]
+    >>> list(rank_data(scalars))
+    [Rank_Data(rank_seq=(1.0,), raw=0.8), Rank_Data(rank_seq=(2.5,), raw=1.2), Rank_Data(rank_seq=(2.5,), raw=1.2), Rank_Data(rank_seq=(4.0,), raw=2.3), Rank_Data(rank_seq=(5.0,), raw=18)]
+
+    >>> pairs= ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
+    >>> rank_x= tuple(rank_data(pairs, key=lambda x:x[0] ))
+    >>> rank_x
+    (Rank_Data(rank_seq=(1.0,), raw=(2, 0.8)), Rank_Data(rank_seq=(2.0,), raw=(3, 1.2)), Rank_Data(rank_seq=(3.0,), raw=(5, 1.2)), Rank_Data(rank_seq=(4.0,), raw=(7, 2.3)), Rank_Data(rank_seq=(5.0,), raw=(11, 18)))
+    >>> rank_xy= (rank_data(rank_x, key=lambda x:x[1] ))
+    >>> tuple(rank_xy)
+    (Rank_Data(rank_seq=(1.0, 1.0), raw=(2, 0.8)), Rank_Data(rank_seq=(2.0, 2.5), raw=(3, 1.2)), Rank_Data(rank_seq=(3.0, 2.5), raw=(5, 1.2)), Rank_Data(rank_seq=(4.0, 4.0), raw=(7, 2.3)), Rank_Data(rank_seq=(5.0, 5.0), raw=(11, 18)))
+    """
+    # Not a sequence? Materialize a sequence object
+    if isinstance(seq_or_iter,collections.abc.Iterator):
+        #for row in rank_data( tuple(seq_or_iter), key ): yield row
+        yield from rank_data( tuple(seq_or_iter), key )
+        return
+    data = seq_or_iter
+    head= seq_or_iter[0]
+    # Collection of non-Rank_Data? Convert to Rank_Data and process.
+    if not isinstance( head, Rank_Data ):
+        ranked= tuple( Rank_Data((),d) for d in data )
+        for r, rd in rerank( ranked, key ):
+            yield Rank_Data( rd.rank_seq+(r,), rd.raw )
+        return
+    # Collection of Rank_Data is what we prefer.
+    for r, rd in rerank( data, key ):
+        yield Rank_Data( rd.rank_seq+(r,), rd.raw )
+
+def rerank( rank_data_collection, key ):
+    """Re-rank by adding another rank order to a Rank_Data object.
+    """
+    sorted_iter= iter( sorted( rank_data_collection,
+        key=lambda obj: key(obj.raw) ) )
+    # Apply ranker to head,*tail = sorted(all)
+    head = next(sorted_iter)
+    #for rows in ranker( sorted_iter, 0, [head], key ): yield rows
+    yield from ranker( sorted_iter, 0, [head], key )
+
+def yield_sequence( rank, same_rank_iter ):
+    """Emit a sequence of same rank values."""
+    head= next(same_rank_iter)
+    yield rank, head
+    #for rest in yield_sequence( rank, same_rank_iter ):
+    #    yield rest
+    yield from yield_sequence( rank, same_rank_iter )
+
+def ranker( sorted_iter, base, same_rank_seq, key ):
+    """Rank values from a sorted_iter using a base rank value.
+    If the next value's key matches same_rank_seq, accumulate those.
+    If the next value's key is different, accumulate same rank values
+    and start accumulating a new sequence.
+
+    >>> scalars= [0.8, 1.2, 1.2, 2.3, 18]
+    >>> list(rank_data(scalars))
+    [Rank_Data(rank_seq=(1.0,), raw=0.8), Rank_Data(rank_seq=(2.5,), raw=1.2), Rank_Data(rank_seq=(2.5,), raw=1.2), Rank_Data(rank_seq=(4.0,), raw=2.3), Rank_Data(rank_seq=(5.0,), raw=18)]
+    """
+    try:
+        value= next(sorted_iter)
+    except StopIteration:
+        dups= len(same_rank_seq)
+        #for rest in yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) ):
+        #    yield rest
+        yield from yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) )
+        return
+    if key(value.raw) == key(same_rank_seq[0].raw):
+        #for rows in ranker(sorted_iter, base, same_rank_seq+[value], key ):
+        #    yield rows
+        yield from ranker(sorted_iter, base, same_rank_seq+[value], key )
+    else:
+        dups= len(same_rank_seq)
+        #for rest in yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) ):
+        #    yield rest
+        yield from yield_sequence( (base+1+base+dups)/2, iter(same_rank_seq) )
+        #for rows in ranker(sorted_iter, base+dups, [value], key): yield rows
+        yield from ranker(sorted_iter, base+dups, [value], key)
+        
+scalars= [0.8, 1.2, 1.2, 2.3, 18]
+list(rank_data(scalars))
